aae0a3b8b2b787bab6d4af0670400e9092f1407a
Nate Wiger <nate@wiger.org>
2012-10-19 16:43:58 -0700

    swirls

--- _plugins/portfolio_generator.rb
+++ _plugins/portfolio_generator.rb
@@ -8,6 +8,36 @@ module Jekyll
     safe true
 
     def generate(site)
+      generate_via_conf(site)
+    rescue => e
+      puts "!!! PortfolioGenerator caught exception: #{e}"
+    end
+
+    def generate_via_conf(site)
+      site.config['portfolio'] = []
+      dir  = site.config['portfolio_dir'] || 'portfolio'
+      suf  = site.config['portfolio_image_suffixes'] ||
+        %w[jpg jpeg JPG JPEG png PNG tiff TIFF pdf PDF]
+      conf = File.expand_path('portfolio.txt', dir)
+
+      File.open(conf).each do |line|
+        # Avoid comments, headers, etc
+        next unless line =~ /^\s*\S+\.(#{suf * '|'})\b/
+
+        f, title, caption = line.strip.split(/\s*\|\s*/, 3)
+
+        # Add to the portfolio for the template
+        site.config['portfolio'] << {
+          'id'      => f.gsub(/\W+/,'_'),
+          'image'   => "/#{dir}/#{f}",
+          'title'   => title,
+          'caption' => caption
+        }
+      end
+    end
+
+    # Unused
+    def generate_via_glob(site)
       site.config['portfolio'] = []
       dir = site.config['portfolio_dir'] || 'portfolio'
       suf = site.config['portfolio_image_suffixes'] ||
@@ -18,6 +48,7 @@ module Jekyll
         %w[txt TXT html HTML htm HTM]
 
       glob = "#{dir}/**.{#{suf*','}}"
+
       #puts "glob = #{glob} ; dir = #{dir} PWD = #{Dir.pwd}"
       Dir[glob].sort.each do |f|
         #puts "portfolio image = #{f}"
@@ -37,13 +68,13 @@ module Jekyll
         site.config['portfolio'] << {
           'id'      => f.gsub(/\W+/,'_'),
           'image'   => f,
-          'caption' => caption,
           'title'   => title,
+          'caption' => caption
         }
       end
-    rescue => e
-      puts "!!! PortfolioGenerator caught exception: #{e}"
     end
+
+
     #def write_tag_index(site, dir, tag)
     #  index = TagIndex.new(site, site.source, dir, tag)
     #  index.render(site.layouts, site.site_payload)
--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ $(function(){
   var er=[115,97,114,97,104,115,97,114,97,102,105,110,97,64,103,109,97,105,108,46,99,111,109];
   var em='';
   for (var i=0; i<er.length; i++) em+=String.fromCharCode(er[i]);
-  $('#mailto').html('<a href="mailto:'+em+'">'+ $('#mailto').html() +'</a>');
+  $('#mailto').html('<a href="mailto:'+em+'">'+ em +'</a>');
 
   // phone too
   var pr=[54,49,57,45,55,55,48,45,48,51,53,49];
